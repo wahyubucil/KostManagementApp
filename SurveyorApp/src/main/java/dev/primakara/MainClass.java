e6c337e0b101a8365bc4de82b25f21f59c478543
Dody <prasetyodody17@gmail.com>
2019-02-15 16:49:33 +0800

    redesign login UI for Surveyor App

--- SurveyorApp/src/main/java/dev/primakara/MainClass.java
+++ SurveyorApp/src/main/java/dev/primakara/MainClass.java
@@ -10,10 +10,21 @@ package dev.primakara;
  * @author hellyeah
  */
 public class MainClass {
+    static boolean isLogin = false;
     static MainForm objMainForm = new MainForm();
     static LoginForm objLoginForm = new LoginForm();
     
     public static void main(String []args){
-        MainClass.objMainForm.setVisible(true);
+        MainClass.loginCheck();
+    }
+    
+    public static void loginCheck() {
+        
+        if (MainClass.isLogin) {
+            MainClass.objMainForm.setVisible(true);
+            MainClass.objLoginForm.dispose();
+        } else {
+            MainClass.objLoginForm.setVisible(true);
+        }
     }
 }
